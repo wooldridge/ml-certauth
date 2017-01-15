5130692fa8a221213a32f422e0ecceb463101bec
Mike Wooldridge <mike@hellion.org>
2017-01-15 13:16:26 -0800

    mlCertAuth module

---
+++ mlCertAuth.js
@@ -0,0 +1,135 @@
+var config = require('./config'),
+    fs = require('fs'),
+    path = require('path'),
+    spawn = require( 'child_process' ).spawn,
+    marklogic = require('marklogic');
+
+var db = marklogic.createDatabaseClient({
+  host: config.host,
+  port: config.database.port,
+  user: config.auth.user,
+  password: config.auth.pass,
+  database: 'Security',
+  authType: 'digest'
+});
+
+function getScript(script) {
+  return fs.readFileSync(
+    process.cwd() + '/scripts/' + script,
+    {encoding: 'utf8'}
+  );
+}
+
+function xqueryEval(src, extVars) {
+  return db.xqueryEval({
+    source: src,
+    variables: extVars
+  }).result()
+}
+
+function createKeys() {
+  var script = getScript('createKeys.sjs');
+  return xqueryEval(script, {});
+}
+
+function createCA(options) {
+  // Save to current directory by default
+  if (!options.path) {
+    options.path = path.dirname(require.main.filename) + "/";
+  }
+  var script = getScript('createCA.xqy');
+  return xqueryEval(script, options);
+}
+
+function createTemplate(options) {
+  var script = getScript('createTemplate.xqy');
+  return xqueryEval(script, options);
+}
+
+function createHostCert(options) {
+  var script = getScript('createHostCert.xqy');
+  return xqueryEval(script, options);
+}
+
+function createUserCert(options) {
+  // Save to current directory by default
+  if (!options.path) {
+    options.path = path.dirname(require.main.filename) + "/";
+  }
+  var script = getScript('createUserCert.xqy');
+  return xqueryEval(script, options);
+}
+
+function createUserPKCS12(options) {
+  // Save to current directory by default
+  if (!options.path) {
+    options.path = path.dirname(require.main.filename) + "/";
+  }
+  var openssl = spawn( 'openssl',
+    [ 'pkcs12', '-export',
+      '-in', options.path + 'certs/' + options.cert,
+      '-inkey', options.path + 'keys/' + options.privkey,
+      '-out', options.path + 'certs/' + options.pfx ]
+  );
+  openssl.stdout.on( 'data', data => {
+    console.log( `stdout: ${data}` );
+  });
+  openssl.stderr.on( 'data', data => {
+    console.log( `stderr: ${data}` );
+  });
+  openssl.on( 'close', code => {
+    console.log( `child process exited with code ${code}` );
+  });
+}
+
+function deleteCert(options) {
+  var script = getScript('deleteCert.xqy');
+  return xqueryEval(script, options);
+}
+
+function deleteCred(options) {
+  var script = getScript('deleteCred.xqy');
+  return xqueryEval(script, options);
+}
+
+function deleteTemplate(options) {
+  var script = getScript('deleteTemplate.xqy');
+  return xqueryEval(script, options);
+}
+
+function getHostCert(options) {
+  var script = getScript('getHostCert.xqy');
+  return xqueryEval(script, options);
+}
+
+function deleteKeys() {
+  var keysPath = path.dirname(require.main.filename) + "/keys/";
+  var keys = fs.readdirSync(keysPath);
+  var rm = spawn( 'rm',
+      [ '-r', keysPath + 'portalpriv.pem' ] );
+  rm.stdout.on( 'data', data => {
+    console.log( `stdout: ${data}` );
+  });
+  rm.stderr.on( 'data', data => {
+    console.log( `stderr: ${data}` );
+  });
+  rm.on( 'close', code => {
+    console.log( `child process exited with code ${code}` );
+  });
+}
+
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+  module.exports = {
+      createKeys: createKeys,
+      createCA: createCA,
+      createTemplate: createTemplate,
+      createHostCert: createHostCert,
+      createUserCert: createUserCert,
+      createUserPKCS12: createUserPKCS12,
+      deleteCert: deleteCert,
+      deleteCred: deleteCred,
+      deleteTemplate: deleteTemplate,
+      getHostCert: getHostCert,
+      deleteKeys: deleteKeys
+  };
+}
