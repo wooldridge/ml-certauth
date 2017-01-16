4702d4efdbded6677b34e7ea4e64ede3ad8aa286
Mike Wooldridge <mike@hellion.org>
2017-01-15 22:48:42 -0800

    Add user creation/deletion to setup and teardown

--- setup.js
+++ setup.js
@@ -112,10 +112,42 @@ function loadData() {
         loadData();
       } else {
         console.log('Data loaded');
+        createUser()
       }
     })
     .catch(function (err) {
       console.log(JSON.stringify(err, null, 2));
+    });
+}
+
+var configUser = {
+  "user-name": config.user.name,
+  "password": config.user.pass,
+  "description": config.user.desc,
+  "role": [ "rest-reader" ]
+};
+
+function createUser() {
+  var options = {
+    method: 'POST',
+    uri: 'http://' + config.host + ':8002/manage/v2/users',
+    body: configUser,
+    json: true,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    auth: config.auth
+  };
+  rp(options)
+    .then(function (parsedBody) {
+      console.log('User created: ' + config.user.name);
+    })
+    .catch(function (err) {
+      if (err.statusCode === 400) {
+        console.log('error - user ' + config.user.name + ' may already exist')
+      } else {
+        console.log(JSON.stringify(err, null, 2));
+      }
     });
 }
 
