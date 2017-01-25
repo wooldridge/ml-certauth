c8fbb2e221d8929347ff974c193ac4669191b11f
Mike Wooldridge <mike@hellion.org>
2017-01-25 00:32:24 -0800

    Rewrite createPKCS12() method to use Promise, take passphrase as option

--- examples/createPKCS12.js
+++ examples/createPKCS12.js
@@ -1,9 +1,16 @@
 var certAuth = require('../mlCertAuth');
 
 var optsPKCS12 = {
+  "passphrase": "p",
   "cert": "client.crt",
   "privkey": "clientpriv.pem",
   "pfx": "clienttest.pfx"
 };
 
-certAuth.createPKCS12(optsPKCS12);
+certAuth.createPKCS12(optsPKCS12)
+  .then(function (result) {
+    console.log('PKCS12 created');
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
